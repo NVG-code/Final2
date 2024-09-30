d4323997b04f7b8af6e393e85c598093d9e7b9dc
NVG-code <113471760+NVG-code@users.noreply.github.com>
2024-09-30 17:02:08 +0200

    Add files via upload

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,25 +1,87 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-const users = require("../src/data/users.json");
-const hosts = require("../src/data/hosts.json");
-const properties = require("../src/data/properties.json");
-const amenities = require("../src/data/amenities.json");
-const bookings = require("../src/data/bookings.json");
-const reviews = require("../src/data/reviews.json");
+import usersData from "../src/data/users.json" assert { type: "json" };
+import hostsData from "../src/data/hosts.json" assert { type: "json" };
+import propertiesData from "../src/data/properties.json" assert { type: "json" };
+import amenitiesData from "../src/data/amenities.json" assert { type: "json" };
+import bookingsData from "../src/data/bookings.json" assert { type: "json" };
+import reviewsData from "../src/data/reviews.json" assert { type: "json" };
 
 async function main() {
-  await prisma.user.createMany({ data: users });
-  await prisma.host.createMany({ data: hosts });
-  await prisma.amenity.createMany({ data: amenities });
-
-  for (const property of properties) {
-    await prisma.property.create({ data: property });
+  try {
+    const { users } = usersData;
+    console.log("users:", users);
+    for (const user of users) {
+      await prisma.user.create({ data: user });
+    }
+    console.log("Data succesvol geïmporteerd voor user");
+  } catch (error) {
+    console.error("Error bij het importeren van users.json:", error);
   }
 
-  await prisma.booking.createMany({ data: bookings });
-  await prisma.review.createMany({ data: reviews });
-}
+  try {
+    const { hosts } = hostsData;
+    for (const host of hosts) {
+      await prisma.host.create({ data: host });
+    }
+    console.log("Data succesvol geïmporteerd voor hosts");
+  } catch (error) {
+    console.error("Error bij het importeren van hosts.json:", error);
+  }
 
+  try {
+    const { properties } = propertiesData;
+    for (const property of properties) {
+      //await prisma.property.create({ data: property });
+      await prisma.property.upsert({
+        where: { id: property.id },
+        update: {},
+        create: property,
+      });
+    }
+    console.log("Data succesvol geïmporteerd voor properties");
+  } catch (error) {
+    console.error("Error bij het importeren van properties.json:", error);
+  }
+
+  try {
+    const { amenities } = amenitiesData;
+    for (const amenity of amenities) {
+      await prisma.amenity.create({ data: amenity });
+    }
+    console.log("Data succesvol geïmporteerd voor amenities");
+  } catch (error) {
+    console.error("Error bij het importeren van amenities.json:", error);
+  }
+
+  try {
+    const { bookings } = bookingsData;
+    // console.log("bookings:", bookings);
+    // console.log("bookingsData:", bookingsData);
+    for (const booking of bookings) {
+      console.log("booking:", booking);
+      await prisma.booking.create({ data: booking });
+    }
+    console.log("Data succesvol geïmporteerd voor bookings");
+  } catch (error) {
+    console.error("Error bij het importeren van bookings.json:", error);
+  }
+
+  try {
+    const { reviews } = reviewsData;
+    for (const review of reviews) {
+      //await prisma.review.create({ data: review });
+      await prisma.review.upsert({
+        where: { id: review.id },
+        update: {},
+        create: review,
+      });
+    }
+    console.log("Data succesvol geïmporteerd voor reviews");
+  } catch (error) {
+    console.error("Error bij het importeren van reviews.json:", error);
+  }
+}
 main()
   .catch((e) => {
     console.error(e);
